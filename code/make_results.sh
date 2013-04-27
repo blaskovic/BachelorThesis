3ff1cd2ee150631ee1bceb5fd61c10f29012234f
Branislav Blaskovic <branislav@blaskovic.sk>
2013-04-27 23:07:52 +0200

    pridane priemery do vysledkov

--- code/make_results.sh
+++ code/make_results.sh
@@ -25,12 +25,18 @@ echo "%"
 for fs in $FILESYSTEMS
 do
 
+    COUNT=0
+    TUNED_SUM=0
+    NOTUNED_SUM=0
+    DIFF_SUM=0
+    PERCENT_SUM=0
+
     # Header of table
     echo "\\begin{table}[H]"
     echo "\\begin{center}"
-    echo "\\begin{tabular}{|l|r|r|r|}"
+    echo "\\begin{tabular}{|l|r|r|r|r|}"
     echo "    \\hline"
-    echo "    \\textbf{Test} & \\textbf{bez tuned} & \\textbf{s tuned} & \\textbf{rozdiel} \\\\ \\hline"
+    echo "    \\textbf{Test} & \\textbf{bez tuned} & \\textbf{s tuned} & \\textbf{rozdiel} & \\textbf{rozdiel [\%]} \\\\ \\hline"
     
     # List results
     for testName in $TESTS
@@ -40,15 +46,29 @@ do
             # Get data
             tuned=$( tlFileLogGet $logFile "tuned-$testName" "$disk-$fs-total-time" )
             notuned=$( tlFileLogGet $logFile "notuned-$testName" "$disk-$fs-total-time" )
+            
+            # Some math
+            let COUNT++
+            TUNED_SUM=$(($TUNED_SUM + $tuned))
+            NOTUNED_SUM=$(($NOTUNED_SUM + $notuned))
+            DIFF_SUM=$(($DIFF_SUM + $notuned - $tuned))
+            PERCENT_SUM=$(($PERCENT_SUM + 100 - 100 * $tuned / $notuned))
 
             # Write to table
             echo -n "    "
             echo -n "${testName//_/\_} $disk $fs & "
-            echo -n "$notuned s & $tuned s & $(($notuned - $tuned)) s "
+            echo -n "${notuned}s & ${tuned}s & $(($notuned - $tuned))s & $((100-100*$tuned/$notuned))\% "
             echo "\\\\"
             echo "    \\hline"
         done
     done
+
+    # Averages
+    echo -n "    "
+    echo -n "\\textbf{Priemery} & "
+    echo -n "`bc <<< \"scale=2;$NOTUNED_SUM / $COUNT\"`s & `bc <<< \"scale=2;$TUNED_SUM / $COUNT\"`s & `bc <<< \"scale=2;$DIFF_SUM / $COUNT\"`s & `bc <<< \"scale=2;$PERCENT_SUM/$COUNT\"`\% "
+    echo "\\\\"
+    echo "    \\hline"
 
     echo "\\end{tabular}"
     echo "\\caption{Výsledky testov pre súborový systém $fs}"
