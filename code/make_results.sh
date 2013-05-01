458244df938ea5b998b6fa04a0d58d73d6ea79c6
Branislav Blaskovic <branislav@blaskovic.sk>
2013-05-02 01:24:47 +0200

    generacia vysledkov - nacitavanie configu

--- code/make_results.sh
+++ code/make_results.sh
@@ -15,67 +15,80 @@ test -f $logFile || { echo "Log file '$logFile' does not exists"; exit 1; }
 # Include functions
 . fun_logger.sh
 
-TESTS="simple_disk raid0 raid1"
-DISKS="virtio"
-FILESYSTEMS="ext3 ext4"
+# Include config
+. config
 
 echo "%"
 echo "% START Automatic results"
 echo "%"
-for fs in $FILESYSTEMS
+
+for profile in $TUNED_PROFILES
 do
+    # Heading
+    echo "\\subsection{Testovanie s profilom \\emph{$profile}}"
 
-    COUNT=0
-    TUNED_SUM=0
-    NOTUNED_SUM=0
-    DIFF_SUM=0
-    PERCENT_SUM=0
-
-    # Header of table
-    echo "\\begin{table}[H]"
-    echo "\\begin{center}"
-    echo "\\begin{tabular}{|l|r|r|r|r|}"
-    echo "    \\hline"
-    echo "    \\textbf{Test} & \\textbf{bez tuned} & \\textbf{s tuned} & \\textbf{rozdiel} & \\textbf{rozdiel [\%]} \\\\ \\hline"
-    
-    # List results
-    for testName in $TESTS
-    do
-        for disk in $DISKS
-        do
-            # Get data
-            tuned=$( tlFileLogGet $logFile "tuned-$testName" "$disk-$fs-total-time" )
-            notuned=$( tlFileLogGet $logFile "notuned-$testName" "$disk-$fs-total-time" )
-            
-            # Some math
-            let COUNT++
-            TUNED_SUM=$(($TUNED_SUM + $tuned))
-            NOTUNED_SUM=$(($NOTUNED_SUM + $notuned))
-            DIFF_SUM=$(($DIFF_SUM + $notuned - $tuned))
-            PERCENT_SUM=$(($PERCENT_SUM + 100 - 100 * $tuned / $notuned))
-
-            # Write to table
-            echo -n "    "
-            echo -n "${testName//_/\_} $disk $fs & "
-            echo -n "${notuned} s & ${tuned} s & $(($notuned - $tuned)) s & `bc <<< \"scale=2;100-100*$tuned/$notuned\"` \% "
-            echo "\\\\"
-            echo "    \\hline"
-        done
-    done
-
-    # Averages
-    echo -n "    "
-    echo -n "\\textbf{Priemery} & "
-    echo -n "`bc <<< \"scale=2;$NOTUNED_SUM / $COUNT\"` s & `bc <<< \"scale=2;$TUNED_SUM / $COUNT\"` s & `bc <<< \"scale=2;$DIFF_SUM / $COUNT\"` s & `bc <<< \"scale=2;$PERCENT_SUM/$COUNT\"` \% "
-    echo "\\\\"
-    echo "    \\hline"
-
-    echo "\\end{tabular}"
-    echo "\\caption{Výsledky testov pre súborový systém $fs}"
-    echo "\\label{tab:results-$fs}"
-    echo "\\end{center}"
-    echo "\\end{table}"
+    # Include text for results if exists
+    echo "\\IfFileExists{obsah-test-$profile}{\\input{obsah-test-$profile}}{}"
     echo
+
+    for fs in ${!FS[@]}
+    do
+
+        COUNT=0
+        TUNED_SUM=0
+        NOTUNED_SUM=0
+        DIFF_SUM=0
+        PERCENT_SUM=0
+
+        # Header of table
+        echo "\\begin{table}[H]"
+        echo "\\begin{center}"
+        echo "\\begin{tabular}{|l|r|r|r|r|}"
+        echo "    \\hline"
+        echo "    \\textbf{Test} & \\textbf{bez tuned} & \\textbf{s tuned} & \\textbf{rozdiel} & \\textbf{rozdiel [\%]} \\\\ \\hline"
+        
+        # List results
+        for testName in $TO_TEST
+        do
+            for disk in $DISK_TYPE
+            do
+                # Get data
+                tuned=$( tlFileLogGet $logFile "tuned-$profile-$testName" "$disk-$fs-total-time" )
+                notuned=$( tlFileLogGet $logFile "notuned-$testName" "$disk-$fs-total-time" )
+
+                [ x = x$tuned ] && { echo "Error reading 'tuned-$profile-$testName' '$disk-$fs-total-time'" >&2; continue;  }
+                [ x = x$notuned ] && { echo "Error reading 'notuned-$testName' '$disk-$fs-total-time'" >&2; continue;  }
+                
+                # Some math
+                let COUNT++
+                TUNED_SUM=$(($TUNED_SUM + $tuned))
+                NOTUNED_SUM=$(($NOTUNED_SUM + $notuned))
+                DIFF_SUM=$(($DIFF_SUM + $notuned - $tuned))
+                PERCENT_SUM=$(($PERCENT_SUM + 100 - 100 * $tuned / $notuned))
+
+                # Write to table
+                echo -n "    "
+                echo -n "${testName//_/\_} $fs & "
+                echo -n "${notuned} s & ${tuned} s & $(($notuned - $tuned)) s & `bc <<< \"scale=2;100-100*$tuned/$notuned\"` \% "
+                echo "\\\\"
+                echo "    \\hline"
+            done
+        done
+
+        # Averages
+        echo -n "    "
+        echo -n "\\textbf{Priemery} & "
+        echo -n "`bc <<< \"scale=2;$NOTUNED_SUM / $COUNT\"` s & `bc <<< \"scale=2;$TUNED_SUM / $COUNT\"` s & `bc <<< \"scale=2;$DIFF_SUM / $COUNT\"` s & `bc <<< \"scale=2;$PERCENT_SUM/$COUNT\"` \% "
+        echo "\\\\"
+        echo "    \\hline"
+
+        echo "\\end{tabular}"
+        echo "\\caption{Výsledky testov pre súborový systém $fs}"
+        echo "\\label{tab:results-$fs}"
+        echo "\\end{center}"
+        echo "\\end{table}"
+        echo
+    done
 done
 echo "%"
 echo "% END Automatic results"
